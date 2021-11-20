a851b3b0d9832dfb3cb831fa5d4ac4665bee855d
Balaji Jayasankar <balaji.jayasankar@gmail.com>
2021-11-20 00:53:16 -0500

    Completed pseudocode for test generator

--- twins_pseudocode/test_generator.py
+++ twins_pseudocode/test_generator.py
@@ -1,10 +1,10 @@
 import random
 
 # Generates a TestCase for an input test_config from the user
-from twins.models.models import TestCase, LeaderPartition
+from twins_pseudocode.models import TestCase, LeaderPartition, TestConfig
 
 
-def generate_test_case(test_config):
+def generate_test_case(test_config: TestConfig):
     # Applies seed if present
     if test_config.seed is not None:
         random.seed(test_config.seed)
@@ -18,46 +18,42 @@ def generate_test_case(test_config):
         validator_twin_ids.append(str(twin_id) + "_twin")
 
     # Generate all possible partition scenarios as described in Step 1 of 4.2 in the Twins paper
-    partition_scenarios = generate_partition_scenarios(validator_twin_ids, test_config.max_partitions)
+    partition_scenarios = generate_partition_scenarios(validator_twin_ids, test_config.n_partitions)
     # For each partition scenario, enumerate all possible intra-partition message drop scenarios
     partition_scenarios = enumerate_partition_scenarios_with_drops(
-        partition_scenarios, test_config.n_message_drop_types)
+        partition_scenarios, test_config.n_intra_drop_types)
     # Generates all possible leader partitions as described in Step 2 of 4.2 in the Twins paper
     all_leader_partitions = generate_leader_partitions(partition_scenarios, test_config.n_validators)
 
-    # Initializes test_case object
-    test_case = TestCase()
-    test_case.n_rounds = test_config.n_rounds
-    test_case.n_validators = test_config.n_validators
-    test_case.twin_ids = twin_ids
 
-    # The leader partition scenarios that we will include in this testcase. Size will be n_rounds
-    leader_partitions = []
-
-    # In case it is deterministic, we will be picking from our enumeration of leader partitions
-    # from start to end
+    # Generates leader_partition_permutations deterministically or randomly based on config
     if test_config.is_deterministic:
-        leader_partition_ptr = 0
+        leader_partition_permutations = enumerate_test_cases(
+            all_leader_partitions, test_config.n_rounds, test_config.n_testcases)
     else:
-        leader_partition_ptr = None
+        leader_partition_permutations = randomly_generate_test_cases(
+            all_leader_partitions, test_config.n_rounds, test_config.n_testcases)
 
-    # Keeps appending leader_partition to the list until we generate for n_rounds.
-    while len(leader_partitions) < test_config.n_rounds:
-        # If test case is generated randomly, we use random choice with replacement
-        # Else we pick from the first element onwards of all permutations
-        if leader_partition_ptr is None:
-            leader_partition = random.choice(all_leader_partitions)
-        else:
-            leader_partition = all_leader_partitions[leader_partition_ptr]
-            leader_partition_ptr += 1
+    # Wraps each leader_partition_permutation into a TestCase object
+    test_cases = [
+        TestCase(test_config.n_rounds, test_config.n_validators, leader_partition_permutation, twin_ids)
+        for leader_partition_permutation in leader_partition_permutations]
 
-        # To ensure some progress in our system, we keep a condition that every 3rd round
-        # has to have some sort of Quorum possible
-        if has_quorum(leader_partition, test_config.f) or len(leader_partitions) % 3 != 0:
-            leader_partitions.append(leader_partition)
+    # Save all testcases to a file
+    write_to_file(test_cases, test_config.path)
 
-    test_case.leader_partitions = leader_partitions
-    return test_case
+
+# Enumerates all possible ways of permuting n_rounds number of leader_partitions
+# with replacement. Terminates when we have generated n_rounds worth of permutations
+def enumerate_test_cases(leader_partitions, n_rounds, n_testcases):
+    # todo
+    return []
+
+
+# Enumerates n_testcases number of leader_partition selection over n_rounds by random selection.
+# For each test case, we pick n_rounds number of leader_partition with replacement
+def randomly_generate_test_cases(leader_partitions, n_rounds, n_testcases):
+    return [[random.choice(leader_partitions) for _ in range(n_rounds)] for _ in range(n_testcases)]
 
 
 def generate_leader_partitions(partition_scenarios, n_validators):
@@ -87,18 +83,8 @@ def enumerate_partition_scenarios_with_drops(partition_scenarios, n_message_drop
     # todo
     return []
 
-# Checks if the validators in any of the partitions in the leader_partition can form a quorum
-def has_quorum(leader_partition, f):
-    if len(leader_partition.dropped_messages) > 0:
-        return False
 
-    for partition in leader_partition.partitions:
-        unique_validators = set()
-
-        for validator_id in partition:
-            unique_validators.add(validator_id.lstrip("_twin"))
-
-        if len(unique_validators) >= 2 * f + 1:
-            return True
-
-    return False
+# Writes the generated test_cases to a file at the specified path
+def write_to_file(test_cases, path):
+    # todo
+    pass
