75d8e0c6fe1743e62bf852a8460095aff5719f69
Prashant Srivastava <prashantsrivastava.knowledge@gmail.com>
2021-11-21 14:12:54 -0500

    Generate cryptographic keys for all validators
    Generate cryptographic keys for all validators

---
+++ twins_pseudocode/crypto.py
@@ -0,0 +1,9 @@
+def generate_key_pairs(n_validators: int):
+    public_private_keys = {}
+    validator_ids = range(1, n_validators + 1)
+    for validator_id in validator_ids:
+        key_pair = cryptography.generate_key()
+        public_private_keys[validator_id]["private_key"] = key_pair[0]
+        public_private_keys[validator_id]["public_key"] = key_pair[1]
+
+    return public_private_keys
